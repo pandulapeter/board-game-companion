b5fffe796f64b638881836b5e4368cad48e10931
Péter Pandula <peter@halcyonmobile.com>
2019-03-24 13:21:25 +0200

    Integrate Crashlytics.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Board Game Companion app
 Work in progress.
+
+### How to build
+The **master** branch should always be buildable, you just need to copy your **google-services.json** file into the **app** folder after cloning the project for the Firebase configuration to work (as this file contains sensitive information, it is not part of the repository).
  
 ### License
 This software is licensed under GNU GPL 3.0. Any derivative works must follow the same open-source license. 
--- app/src/main/java/com/rbtgames/boardgame/Application.kt
+++ app/src/main/java/com/rbtgames/boardgame/Application.kt
@@ -1,6 +1,8 @@
 package com.rbtgames.boardgame
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.android.startKoin
 
 @Suppress("unused")
@@ -8,6 +10,10 @@ class Application : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        @Suppress("ConstantConditionIf")
+        if (BuildConfig.BUILD_TYPE != "debug") {
+            Fabric.with(this, Crashlytics())
+        }
         startKoin(
             this, listOf(
                 persistenceModule,
