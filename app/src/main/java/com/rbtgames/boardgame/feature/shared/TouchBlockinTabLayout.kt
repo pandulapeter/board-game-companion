838be4d59dcf65377904c55e28785765fe02cd4b
Péter Pandula <peter@halcyonmobile.com>
2019-04-27 17:45:30 +0300

    Fix tab bar scrolling issue.

--- app/src/main/java/com/rbtgames/boardgame/feature/shared/TouchBlockinTabLayout.kt
+++ app/src/main/java/com/rbtgames/boardgame/feature/shared/TouchBlockinTabLayout.kt
@@ -1,5 +1,6 @@
 package com.rbtgames.boardgame.feature.shared
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
@@ -9,4 +10,7 @@ import com.google.android.material.tabs.TabLayout
 open class TouchBlockinTabLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : TabLayout(context, attrs) {
 
     override fun onInterceptTouchEvent(ev: MotionEvent?) = false
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(ev: MotionEvent?) = false
 }
