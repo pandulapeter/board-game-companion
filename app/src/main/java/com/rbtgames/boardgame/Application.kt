fa2572383bab4643dfdcdc43589e898a923b3689
Péter Pandula <peter@halcyonmobile.com>
2019-04-28 14:20:52 +0300

    Add LeakCanary dependency.

--- app/src/main/java/com/rbtgames/boardgame/Application.kt
+++ app/src/main/java/com/rbtgames/boardgame/Application.kt
@@ -2,6 +2,7 @@ package com.rbtgames.boardgame
 
 import android.app.Application
 import com.crashlytics.android.Crashlytics
+import com.squareup.leakcanary.LeakCanary
 import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.android.startKoin
 
@@ -10,6 +11,10 @@ class Application : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return
+        }
+        LeakCanary.install(this)
         @Suppress("ConstantConditionIf")
         if (BuildConfig.BUILD_TYPE != "debug") {
             Fabric.with(this, Crashlytics())
